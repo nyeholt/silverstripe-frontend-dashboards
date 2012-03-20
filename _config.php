5a9bd65442f8b15b98778d40a19c38c0a7c98bfd
Marcus Nyeholt <marcus@silverstripe.com.au>
2012-03-20 21:50:45 +1100

    Remove restrictable requirement and root url routing

--- _config.php
+++ _config.php
@@ -1,12 +1,13 @@
 <?php
 
 Director::addRules(40, array(
-	''						=> 'DashboardController',
+// uncomment the following for all requests to route to the dashboard controller
+// 	''					=> 'DashboardController',
 	'dashboard'				=> 'DashboardController',
 ));
 
 Object::add_extension('Member', 'DashboardUser');
-Object::add_extension('Dashlet',	'Restrictable');
+// Object::add_extension('Dashlet',	'Restrictable');
 Object::add_extension('DateField', 'DateFieldExtension');
 
-include_once dirname(__FILE__).'/code/db/LoggingSQLite3Database.php';
+// include_once dirname(__FILE__).'/code/db/LoggingSQLite3Database.php';
